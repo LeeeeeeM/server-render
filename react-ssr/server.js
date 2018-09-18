f6e3cd9600a515fb5f93359e397efe7f3e7a04d1
LeeeeeeM <sunliangmu@xiaomi.com>
2018-09-18 15:38:12 +0800

    增加intial数据解决数据server client textContent不同步的warning

--- react-ssr/server.js
+++ react-ssr/server.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import express from 'express'
+import cors from 'cors'
+import fs from 'fs'
+import path from 'path'
+import serialize from 'serialize-javascript'
 import {
   renderToString
 } from 'react-dom/server'
 import App from './src/App'
 import getFacts from './src/facts'
-import express from 'express'
-import cors from 'cors'
-import fs from 'fs'
-import path from 'path'
 
 const app = new express()
 const indexPage = fs.readFileSync(path.resolve(process.cwd(), 'index.html'), 'utf-8')
@@ -26,7 +27,7 @@ app.get('/facts', cors(), (req, res, next) => {
 app.get('/', (req, res) => {
   getFacts('server').then(facts => {
     const html = renderToString( < App facts = {facts} />)
-    const finalHtml = indexPage.replace('<!-- :: APP :: -->', html)
+    const finalHtml = indexPage.replace('<!-- :: APP :: -->', html).replace('\'<!-- :: DATA :: -->\'', serialize(facts))
     res.set('Cache-Control', 'public, max-age=600, s-maxage=1200')
     res.send(finalHtml)
   }).catch(e => {
--- react-ssr/src/index.js
+++ react-ssr/src/index.js
@@ -5,10 +5,13 @@ import {
 import App from './App'
 import getFacts from './facts'
 
+hydrate(<App facts = {window.__$INITIAL_DATA__} />,
+  document.querySelector('#root'))
+
 setTimeout(() => {
   getFacts('client').then(facts => {
       hydrate( <App facts = {facts} />,
         document.querySelector('#root'))
       })
-  }, 3000
+  }, 1500
 )
